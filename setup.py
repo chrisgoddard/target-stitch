56cd6f8de4f66bb048deef771e5556e3b385869d
chrisgoddard <chris@chrisgoddard.me>
2019-03-29 10:29:58 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='target-stitch',
-      version='1.8.1',
+      version='1.8.19',
       description='Singer.io target for the Stitch API',
       author='Stitch',
       url='https://singer.io',
